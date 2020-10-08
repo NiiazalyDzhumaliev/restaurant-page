7463b22dfdab1622605d0c39fd4a086517dcc3b4
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-10-08 15:42:02 +0600

    Add main and index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert('Hello world');
+function addElement() {
+  const newPar = document.createElement("P");
+  const newContent = document.createTextNode("Hi there guys");
+  newPar.appendChild(newContent);
+  const currentDiv = document.getElementById("content");
+  currentDiv.appendChild(newPar);
+}
+
+addElement();
