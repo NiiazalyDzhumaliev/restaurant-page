3effec7e18cef1a94e5baf4fab358fba641d9132
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-10-08 19:41:58 +0600

    Add header and navbar part

--- src/modules/header.js
+++ src/modules/header.js
@@ -4,12 +4,23 @@ const loadHeader = () => {
   content.appendChild(headerContainer);
   headerContainer.setAttribute("class", "header-container");
   const headerDiv = document.createElement("div");
-  headerDiv.setAttribute("class", "header-div");  
+  headerDiv.setAttribute("class", "header-div");
   headerContainer.appendChild(headerDiv);
-  const header = document.createElement('h2');
-  header.setAttribute('class', 'header')
-  header.textContent = 'Supara'
+  const header = document.createElement("h2");
+  header.setAttribute("class", "header");
+  header.textContent = "Supara";
   headerDiv.appendChild(header);
+
+  const arr = ["home", "about", "contact"];
+  const navUl = document.createElement("ul");
+  navUl.setAttribute("class", "nav-list");
+  headerContainer.appendChild(navUl);
+  for (let i = 0; i < arr.length; i++) {
+    const navLi = document.createElement("li");
+    navLi.textContent = arr[i];
+    navLi.setAttribute("class", "list-item");
+    navUl.appendChild(navLi);
+  }
 };
 
 export { loadHeader };
