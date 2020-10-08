b82d6b3705c9b116778f715d47d6c4bf9656851b
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-10-08 20:07:07 +0600

    Style navbar content

--- src/modules/header.js
+++ src/modules/header.js
@@ -2,23 +2,23 @@ const content = document.getElementById("content");
 const loadHeader = () => {
   const headerContainer = document.createElement("DIV");
   content.appendChild(headerContainer);
-  headerContainer.setAttribute("class", "header-container");
+  headerContainer.setAttribute("class", "header-container d-flex justify-content-between mt-3");
   const headerDiv = document.createElement("div");
   headerDiv.setAttribute("class", "header-div");
   headerContainer.appendChild(headerDiv);
   const header = document.createElement("h2");
-  header.setAttribute("class", "header");
+  header.setAttribute("class", "header ml-5");
   header.textContent = "Supara";
   headerDiv.appendChild(header);
 
   const arr = ["home", "about", "contact"];
   const navUl = document.createElement("ul");
-  navUl.setAttribute("class", "nav-list");
+  navUl.setAttribute("class", "nav-list d-flex");
   headerContainer.appendChild(navUl);
   for (let i = 0; i < arr.length; i++) {
     const navLi = document.createElement("li");
     navLi.textContent = arr[i];
-    navLi.setAttribute("class", "list-item");
+    navLi.setAttribute("class", "list-item mr-5");
     navUl.appendChild(navLi);
   }
 };
